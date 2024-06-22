2a3cf8be1b87282dfee9e53a99f6f43572a5afae
jzide <jzide@equal.jz>
2024-06-22 13:47:25 +0900

    update

---
+++ postgresql/init.d/init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Pre-Initialize ############################################################
+psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
+	CREATE USER $DATABASE_USER PASSWORD '$POSTGRES_PASSWORD' SUPERUSER;
+	CREATE ROLE repuser WITH REPLICATION LOGIN PASSWORD '$POSTGRES_PASSWORD';
+	COMMIT;
+EOSQL
+
+# Initialize ################################################################
+
+# Create Databases
+psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
+	CREATE DATABASE eqpls OWNER $DATABASE_USER;
+	COMMIT;
+EOSQL
+
+# Set Database Scheme & Data
+psql -v ON_ERROR_STOP=1 --username postgres --dbname eqpls -f /init.d/eqpls.sql
